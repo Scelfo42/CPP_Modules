67cae8546e7c312310366c6ed8fd5c3e8a3d8346
Scelfo42 <rimscelfo.christian@gmail.com>
2023-10-12 16:45:29 +0200

    automatic

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cscelfo <cscelfo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 15:59:30 by cscelfo           #+#    #+#             */
-/*   Updated: 2023/10/11 15:15:50 by cscelfo          ###   ########.fr       */
+/*   Created: 2023/10/12 14:36:14 by cscelfo           #+#    #+#             */
+/*   Updated: 2023/10/12 14:39:52 by cscelfo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int	main( int argc, char *argv[] )
-{
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc; i++)
-		for (int j = 0; argv[i][j] != '\0'; j++)
-			std::cout << (char)std::toupper( argv[i][j] );
-	std::cout << std::endl;
-	return 0;
-}
--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -1,24 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cscelfo <cscelfo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/10 15:59:30 by cscelfo           #+#    #+#             */
-/*   Updated: 2023/10/11 15:15:50 by cscelfo          ###   ########.fr       */
+/*   Created: 2023/10/12 14:37:34 by cscelfo           #+#    #+#             */
+/*   Updated: 2023/10/12 14:39:47 by cscelfo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "PhoneBook.hpp"
 
-int	main( int argc, char *argv[] )
+int main( int argc, char *argv[] )
 {
-	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	for (int i = 1; i < argc; i++)
-		for (int j = 0; argv[i][j] != '\0'; j++)
-			std::cout << (char)std::toupper( argv[i][j] );
-	std::cout << std::endl;
 	return 0;
 }
