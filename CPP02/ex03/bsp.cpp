ada60713d02bacf2954d44dc23df3541705d5248
Scelfo42 <rimscelfo.christian@gmail.com>
2023-11-06 11:19:09 +0100

    Fixed ex03/bsp.cpp when point on edge or vertex

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: cscelfo <cscelfo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 16:54:27 by cscelfo           #+#    #+#             */
-/*   Updated: 2023/11/04 21:30:26 by cscelfo          ###   ########.fr       */
+/*   Updated: 2023/11/06 11:17:57 by cscelfo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,34 @@ static int	calcDet(int det[3][3])
 	diagonal = (det[0][0] * det[1][1] * det[2][2]) + (det[0][1] * det[1][2] * det[2][0]) + (det[0][2] * det[1][0] * det[2][1]);
 	reverseDiagonal = (det[0][2] * det[1][1] * det[2][0]) + (det[0][1] * det[1][0] * det[2][2]) + (det[0][0] * det[1][2] * det[2][1]);
 	result = diagonal - reverseDiagonal;
+
 	return (result >= 0) ? result : -result;
 }
 
 static int subArea( Point const vert1, Point const vert2, Point const vert3 )
 {
-	int	det[3][3] = { { vert1.getX(), vert1.getY(), 1 },
-					{ vert2.getX(), vert2.getY(), 1 },
-					{ vert3.getX(), vert3.getY(), 1 } };
+	int	det[3][3] = {	{ vert1.getX(), vert1.getY(), 1 },
+						{ vert2.getX(), vert2.getY(), 1 },
+						{ vert3.getX(), vert3.getY(), 1 }	};
+
 	return (calcDet(det) / 2);
 }
 
-static bool	checkEdge( Point const a, Point const b, Point const c, Point const point )
+static bool	checkEdge( int area1, int area2, int area3 )
 {
-	if ((a.getX() == point.getX() && a.getY() == point.getY()) ||
-		(b.getX() == point.getX() && b.getY() == point.getY()) ||
-		(c.getX() == point.getX() && c.getY() == point.getY()))
+	if (area1 == 0 || area2 == 0 || area3 == 0)
 		return true;
 	return false;
 }
 
 bool	bsp( Point const a, Point const b, Point const c, Point const point )
 {
-	static int	i = 0;
-
-	i++;
-	if (checkEdge(a, b, c, point))
-		return false;
 	int	masterTriangle = subArea(a, b, c);
-	std::cout << "masterTriangle: " << masterTriangle << std::endl;
 	int	subTriangles[3] = { subArea(a, b, point), subArea(b, c, point), subArea(a, c, point) };
-	std::cout << "subTriangles [" << i << "]: " << subTriangles[0] << " " << subTriangles[1] << " " << subTriangles[2] << std::endl;
 	int sumSubTriangles = subTriangles[0] + subTriangles[1] + subTriangles[2];
-	std::cout << "sumSubTriangles: " << sumSubTriangles << std::endl;
+
+	if (checkEdge(subTriangles[0], subTriangles[1], subTriangles[2]))
+		return false;
+
 	return (masterTriangle == sumSubTriangles);
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cscelfo <cscelfo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:11:07 by cscelfo           #+#    #+#             */
-/*   Updated: 2023/11/04 21:32:35 by cscelfo          ###   ########.fr       */
+/*   Updated: 2023/11/06 11:17:30 by cscelfo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,20 @@ int main ()
 {
     Point   in(3, 1);
     Point   out(10, 10);
-    Point   on(2, 6);
+    Point   vertex(2, 6);
 	Point	edge(2, 0);
-    Point   edge2(1, 0);
-    Point   edge3(3, 0);
     
     bool    insidePoint = bsp( Point(0, 0), Point(2, 6), Point(4, 0), in);
     bool    outsidePoint = bsp( Point(0, 0), Point(2, 6), Point(4, 0), out);
-    bool    vertexPoint = bsp( Point(0, 0), Point(2, 6), Point(4, 0), on);
+    bool    vertexPoint = bsp( Point(0, 0), Point(2, 6), Point(4, 0), vertex);
 	bool	edgePoint = bsp( Point(0, 0), Point(2, 6), Point(4, 0), edge);
-    bool	edgePoint2 = bsp( Point(0, 0), Point(2, 6), Point(4, 0), edge2);
-    bool	edgePoint3 = bsp( Point(0, 0), Point(2, 6), Point(4, 0), edge3);
 
+    std::cout << std::endl;
     std::cout << "Point(3, 1) is " << (insidePoint ? "inside" : "outside") << " the triangle" << std::endl;
     std::cout << "Point(10, 10) is " << (outsidePoint ? "inside" : "outside") << " the triangle" << std::endl;
     std::cout << "Point(2, 6) is " << (vertexPoint ? "inside" : "outside") << " the triangle" << std::endl;
     std::cout << "Point(2, 0) is " << (edgePoint ? "inside" : "outside") << " the triangle" << std::endl;
-    std::cout << "Point(1, 0) is " << (edgePoint2 ? "inside" : "outside") << " the triangle" << std::endl;
-    std::cout << "Point(3, 0) is " << (edgePoint3 ? "inside" : "outside") << " the triangle" << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
